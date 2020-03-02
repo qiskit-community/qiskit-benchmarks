c29840915fe8e7ec63bf1cfd012e815e5d0e43ec
Luciano Bello <luciano.bello@ibm.com>
2020-03-01 22:03:41 -0500

    moving things around

--- test_entry_points.py
+++ test_entry_points.py
@@ -1,9 +1,7 @@
 import sys
-import site
 import os.path
 import importlib
 from datetime import datetime
-from pprint import pprint
 from itertools import chain
 from dataclasses import dataclass, asdict
 from json import dumps, JSONEncoder
@@ -11,7 +9,7 @@ from json import dumps, JSONEncoder
 import yaml
 import pkg_resources
 import importlib_metadata
-from importlib_metadata import distribution, files
+from importlib_metadata import files
 from qiskit import QuantumCircuit
 from qiskit.transpiler.pass_manager_config import PassManagerConfig
 from qiskit.transpiler import CouplingMap
@@ -20,78 +18,82 @@ import benchmark
 
 
 class ResultEncoder(JSONEncoder):
-  def default(self, obj):
-    if isinstance(obj, QuantumCircuit):
-      return obj.name
-    if isinstance(obj, PassManagerConfig):
-      return obj.name
-    if isinstance(obj, CouplingMap):
-      return obj.get_edges()
-    return JSONEncoder.default(self, obj)
+    def default(self, obj):
+        if isinstance(obj, QuantumCircuit):
+            return obj.name
+        if isinstance(obj, PassManagerConfig):
+            return obj.name
+        if isinstance(obj, CouplingMap):
+            return obj.get_edges()
+        return JSONEncoder.default(self, obj)
 
 
 def test_entry_points(contributions_path, results_path):
-  results = {}
+    results = {}
 
-  with open(contributions_path) as file:
-    contributions = yaml.load(file)
+    with open(contributions_path) as file:
+        contributions = yaml.load(file)
 
-  entry_points = chain(*(next(iter(cont.values())) for cont in contributions))
+    entry_points = chain(*(next(iter(cont.values())) for cont in contributions))
 
-  for one_entry_point in entry_points:
-    module_fqn, function_name = one_entry_point.split(':')
-    module = importlib.import_module(module_fqn)
-    metadata = get_metadata(module)
-    try:
-      function = getattr(module, function_name)
-      if metadata.name not in results:
-        results[metadata.name] = { **asdict(metadata), 'results': [] }
+    for one_entry_point in entry_points:
+        module_fqn, function_name = one_entry_point.split(':')
+        module = importlib.import_module(module_fqn)
+        metadata = get_metadata(module)
+        try:
+            function = getattr(module, function_name)
+            if metadata.name not in results:
+                results[metadata.name] = {**asdict(metadata), 'results': []}
 
-      results[metadata.name]['results'].append((one_entry_point, test(function)))
-    except AttributeError:
-      notify_cannot_find_entry_point(metadata.author_email, one_entry_point)
+            results[metadata.name]['results'].append((one_entry_point, test(function)))
+        except AttributeError:
+            notify_cannot_find_entry_point(metadata.author_email, one_entry_point)
 
-  with open(results_path, 'w+') as file:
-    file.write(dumps(results, cls=ResultEncoder))
+    with open(results_path, 'w+') as file:
+        file.write(dumps(results, cls=ResultEncoder))
+
 
 def get_metadata(module):
-  module_path = module.__file__
-  for dist in pkg_resources.working_set:
-    for path in get_dist_files(dist):
-      if module_path == path:
-        meta = importlib_metadata.metadata(dist.project_name)
-        return metadata(
-          name=meta['Name'],
-          author=meta['Author'],
-          author_email=meta['Author-email'],
-          description=meta['Summary'],
-          timestamp=str(datetime.now())
-        )
+    module_path = module.__file__
+    for dist in pkg_resources.working_set:
+        for path in get_dist_files(dist):
+            if module_path == path:
+                meta = importlib_metadata.metadata(dist.project_name)
+                return metadata(
+                    name=meta['Name'],
+                    author=meta['Author'],
+                    author_email=meta['Author-email'],
+                    description=meta['Summary'],
+                    timestamp=str(datetime.now())
+                )
+
 
 @dataclass
 class metadata:
+    name: str
 
-  name: str
+    description: str
 
-  description: str
+    author: str
 
-  author: str
+    author_email: str
 
-  author_email: str
-
-  timestamp: str
+    timestamp: str
 
 
 def get_dist_files(dist):
-  for path in files(dist.project_name):
-    path = os.path.normpath(os.path.join(dist.location, path))
-    yield path
+    for path in files(dist.project_name):
+        path = os.path.normpath(os.path.join(dist.location, path))
+        yield path
+
 
 def test(function):
-  return benchmark.test(function)
+    return benchmark.test(function)
+
 
 def notify_cannot_find_entry_point(email, entry_point):
-  print(f'send "{entry_point} not working" @ {email}')
+    print(f'send "{entry_point} not working" @ {email}')
+
 
 if __name__ == '__main__':
-  test_entry_points(sys.argv[1], sys.argv[2])
+    test_entry_points(sys.argv[1], sys.argv[2])
--- testsuites/random/cases.py
+++ testsuites/random/cases.py
@@ -17,12 +17,19 @@ circuit2.h(qr[0])
 circuit2.cx(qr[1], qr[0])
 
 coupling1 = [[1, 0], [1, 2], [2, 3], [4, 3], [4, 10], [5, 4],
-            [5, 6], [5, 9], [6, 8], [7, 8], [9, 8], [9, 10],
-            [11, 3], [11, 10], [11, 12], [12, 2], [13, 1], [13, 12]]
+             [5, 6], [5, 9], [6, 8], [7, 8], [9, 8], [9, 10],
+             [11, 3], [11, 10], [11, 12], [12, 2], [13, 1], [13, 12]]
 coupling2 = [[1, 0], [1, 2], [2, 3], [4, 3]]
 
-pm_config1 = PassManagerConfig(seed_transpiler=42, basis_gates=['u1', 'u2', 'u3', 'cx', 'id'], coupling_map=CouplingMap(coupling1))
-pm_config2 = PassManagerConfig(seed_transpiler=42, basis_gates=['u1', 'u2', 'u3', 'cx', 'id'], coupling_map=CouplingMap(coupling2))
+pm_config1 = PassManagerConfig(seed_transpiler=42, basis_gates=['u1', 'u2', 'u3', 'cx', 'id'],
+                               coupling_map=CouplingMap(coupling1))
+pm_config2 = PassManagerConfig(seed_transpiler=42, basis_gates=['u1', 'u2', 'u3', 'cx', 'id'],
+                               coupling_map=CouplingMap(coupling2))
 
 pm_config1.name = 'pm_config1'
 pm_config2.name = 'pm_config2'
+
+
+def get_case():
+    return [(circuit1, pm_config1),
+            (circuit2, pm_config2)]
